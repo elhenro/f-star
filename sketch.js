ec153fd2ccf76288b65903e4e2773ed00c201d38
marcolilli <github@einmarco.com>
2018-11-25 16:49:27 +0100

    asd

--- sketch.js
+++ sketch.js
@@ -2,7 +2,9 @@
 var Engine = Matter.Engine,
     Render = Matter.Render,
     World = Matter.World,
-    Bodies = Matter.Bodies;
+    Bodies = Matter.Bodies,
+    Events = Matter.Events,
+    Body = Matter.Body;
 
 // create an engine
 var engine = Engine.create();
@@ -13,16 +15,40 @@ var render = Render.create({
     engine: engine
 });
 
-// create two boxes and a ground
-var boxA = Bodies.rectangle(400, 200, 80, 80);
-var boxB = Bodies.rectangle(450, 50, 80, 80);
-var ground = Bodies.rectangle(400, 610, 810, 60, { isStatic: true });
+engine.world.gravity.y = 0;
+
+// create some boxes
+let boxes = [];
+for (let i = 0; i < 6; i++) {
+    boxes.push(Bodies.rectangle(400, 200, 20, 20));
+}
+
+// create a ground
+var ground = Bodies.rectangle(400, 610, 810, 60, {isStatic: true, frictionAir: 1 });
+
+for (let box of boxes) {
+    console.log(box);
+}
+
+// Get the position of the boxes
+Events.on(engine, 'afterUpdate', function () {
+    /*for (let box of boxes) {
+        console.log(box.position.x, box.position.y);
+    }*/
+});
+
+
+$('.linear').on('click', function () {
+    Body.setVelocity( boxes[0], {x: 2, y: -7});
+});
+
 
 // add all of the bodies to the world
-World.add(engine.world, [boxA, boxB, ground]);
+World.add(engine.world, boxes);
 
 // run the engine
 Engine.run(engine);
 
 // run the renderer
 Render.run(render);
+
